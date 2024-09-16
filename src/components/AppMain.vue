82ccc78a90e2e71f05b94f947800dd8bb82bb6ec
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-09-16 16:09:10 +0200

    structure of AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,7 +9,23 @@ export default {
 </script>
 
 <template>
-    <h2>MAIN</h2>
+    <main>
+        <div class="container">
+            <section id="content">
+                <p>--Content goes here--</p>
+            </section>
+        </div>
+    </main>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+main {
+    background-color: grey;
+    height: 120px;
+
+    #content {
+        line-height: 120px;
+        color: white;
+    }
+}
+</style>
