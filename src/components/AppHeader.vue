a682907092c20b413a6c92c5ff3123d6b3063cfe
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-09-16 16:02:41 +0200

    structure of AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,6 +2,18 @@
 export default {
     data() {
         return {
+            headerLinks: [
+                "Characters",
+                "Comics",
+                "Movies",
+                "Tv",
+                "Games",
+                "Collectible",
+                "Videos",
+                "Fans",
+                "News",
+                "Shop"
+            ]
 
         }
     }
@@ -9,7 +21,57 @@ export default {
 </script>
 
 <template>
-    <h2>HEADER</h2>
+    <header>
+        <div class="container">
+            <!--LOGO-->
+            <section id="logo">
+                LOGO
+            </section>
+
+            <!--LINKS-->
+            <nav id="navbar-links">
+                <ul>
+                    <li v-for="link in headerLinks"><a href="#">{{ link }}</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+header {
+    background-color: aqua;
+    width: 100%;
+    height: 100px;
+
+    .container {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    #logo {
+        background-color: yellow;
+        width: 100px;
+        text-align: center;
+    }
+
+    nav {
+        height: 100%;
+    }
+
+    ul {
+        height: 100%;
+        display: flex;
+
+        li {
+            line-height: 100px;
+            height: 100%;
+            padding: 0 10px;
+
+            border-bottom: 10px solid white;
+        }
+    }
+}
+</style>
