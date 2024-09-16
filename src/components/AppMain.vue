53440602f6adc136a4d8667dbb2598c4488c9396
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-09-16 17:22:04 +0200

    style AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -20,12 +20,14 @@ export default {
 
 <style lang="scss" scoped>
 main {
-    background-color: grey;
-    height: 120px;
+    background-color: #1C1C1C;
+    height: 140px;
 
     #content {
         line-height: 120px;
         color: white;
+        font-size: 2rem;
+        font-weight: bold;
     }
 }
 </style>
